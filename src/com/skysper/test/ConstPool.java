89f5552d176fecc7d1824c80ba533709fcda8fab
吴朋飞 <wupengfei@yiche.com>
2018-10-25 19:49:24 +0800

    init project with Math algs

---
+++ src/com/skysper/test/ConstPool.java
@@ -0,0 +1,13 @@
+package com.skysper.test;
+
+/**
+ * @author wupengfei
+ */
+public class ConstPool {
+    public static  final  int TEN_MILLION = 10_000_000;
+    public static  final  int HUNDRED_MILLION = 100_000_000;
+    public static final int ONE_MILLION = 1_000_000;
+    public static final  int HUNDRED_THOUSAND = 100_000;
+    public static final int TEN_THOUSAND = 10_000;
+    public static final  int ONE_THOUSAND = 1_000;
+}

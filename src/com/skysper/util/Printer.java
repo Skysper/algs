485c48d9c6ca459a2da55ec0dc15339f14ef6b4b
吴朋飞 <wupengfei@yiche.com>
2018-10-31 19:07:31 +0800

    add binary search and printer helper

--- src/com/skysper/util/Printer.java
+++ src/com/skysper/util/Printer.java
@@ -1,5 +1,7 @@
 package com.skysper.util;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 打印帮助类
  * @author wupengfei
@@ -13,6 +15,22 @@ public class Printer {
             System.out.print(" ");
         }
         System.out.println("]");
+    }
+
+    public static void printArray(int[] array) {
+        System.out.print("[");
+        for (int item : array) {
+            System.out.print(item);
+            System.out.print(" ");
+        }
+        System.out.println("]");
+    }
+
+    public static void logDebug(String message) {
+        System.out.println(System.currentTimeMillis() + " [DEBUG] " + message);
+    }
+    public static  void logError(String message) {
+        System.out.println(System.currentTimeMillis() + " [ERROR]" + message);
     }
 
 }

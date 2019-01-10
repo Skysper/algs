c9a931c9b6cff6c990fbac767beb8e260cbee6b9
skysper <wtwpfwd0708@gmail.com>
2019-01-10 13:19:00 +0800

    change main to junit test

--- src/test/com/skysper/algs/MathTest.java
+++ src/test/com/skysper/algs/MathTest.java
@@ -1,40 +1,48 @@
 package com.skysper.algs;
 
 import com.skysper.ConstPool;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
 
 /**
  * @author skysper
  */
+@RunWith(JUnit4ClassRunner.class)
 public class MathTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void testSqrt() {
         int number = 214;
-        MathTest.testSqrt(number);
+        double jdkResult = Math.sqrt(number);
+        System.out.println("result is :" + jdkResult);
+        double mineResult = java.lang.Math.sqrt(number);
+        System.out.println("java.lang.Math result is :" + mineResult);
 
-        MathTest.testGcd(65, 39);
+        Assert.assertEquals(jdkResult, mineResult, 0.0000000001);
     }
 
-    private static void testSqrt(int number) {
-        double result = Math.sqrt(number);
-        System.out.println("result is :" + result);
-        result = java.lang.Math.sqrt(number);
-        System.out.println("java.lang.Math result is :" + result);
+    @Test
+    public void testGcd() {
+        int a =65,b=39;
+        int result = Math.gcd(a, b);
+        Assert.assertEquals(13, result);
+    }
 
+    @Test
+    public void runPerformance(){
+        int number = 214;
         long begin = System.currentTimeMillis();
         for(int i = 0; i < ConstPool.TEN_MILLION; i++) {
             Math.sqrt(number + i/100);
         }
-        System.out.println("use com.skysper.algs.Math, total times:" + (System.currentTimeMillis() - begin));
+        System.out.println("use com.skysper.algs.Math, total times:" + (System.currentTimeMillis() - begin) +"ms");
         begin = System.currentTimeMillis();
         for(int i = 0; i < ConstPool.TEN_MILLION; i++) {
             java.lang.Math.sqrt(number + i/100);
         }
-        System.out.println("use java.lang.Math,total times:" + (System.currentTimeMillis() - begin));
-    }
-
-    private static void testGcd(int a, int b) {
-        int result = Math.gcd(a, b);
-        System.out.println("max gcd is " + result);
+        System.out.println("use java.lang.Math,total times:" + (System.currentTimeMillis() - begin) + "ms");
     }
 
 }
--- src/test/com/skysper/algs/SattoloTest.java
+++ src/test/com/skysper/algs/SattoloTest.java
@@ -1,14 +1,18 @@
 package com.skysper.algs;
 
 import com.skysper.ConstPool;
-import com.skysper.algs.Sattolo;
 import com.skysper.util.Printer;
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnit4ClassRunner.class)
 public class SattoloTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void showResult() {
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for(int i = 0; i < ConstPool.TEN; i++){
-            Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
             Sattolo.shuffle(array);
             Printer.printArray(array);
         }
